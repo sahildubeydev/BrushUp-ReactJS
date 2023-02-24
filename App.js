0230cd046942cbcebca3414c89c97ef73c4135e0
Sahil Dubey <dsahil.contact@gmail.com>
2023-02-24 13:39:30 +0530

    Revised the concepts of scripts, start, build, babel, keys, jsx, element, components

--- App.js
+++ App.js
@@ -15,40 +15,74 @@
  * PORT Number
  * Parcel uses Consist Hashing ALgorithms for Caching
  * It is a Zero Config Bundler
- *
- *
+ * Tree Shaking -Removing nu-wanted
  *
  * Transitive Dependencies
  */
 
-import React from "react";
+import { React } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1 from parcel"
+// We will never use React.createElement
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "Heading 1 from parcel"
+// );
+
+// React.createElement(gives us object) => Object (object is converted to html code) => HTML(DOM)
+
+// const heading = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Namaste React"
+// );
+
+/**<div class="header">
+      <h1>Namaste React</h1>
+      <ul>
+        <li>About Us</li>
+        <li>Support</li>
+        <li>Home</li>
+      </ul>
+    </div> 
+*/
+
+// JSX?
+// What is a React element? Object
+const title = (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
-);
+// React Component
+// - Functional - New way of writing code
+// - Use of functional component -
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    hello: "world",
-  },
-  [heading, heading2]
-);
+// Composing component / Component composition
+// = Passing component inside another component then it is known as component composition.
+const HeaderComponent = () => {
+  return (
+    <div>
+      {title}
+      <h1>Namaste React from functional component</h1>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
+
+// - Class Based Component - Old Way of writing code
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// passing a react element inside the root
+
+// async defer
+root.render(<HeaderComponent />);
